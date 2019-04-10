6986d06e05259493774852342eee271e05e77ad1
Thomas Petchel <tpetchel@gmail.com>
2019-04-10 09:57:28 -0700

    Create failing test

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -111,5 +111,25 @@ namespace Tests
             // Verify that we received the specified number of items.
             Assert.AreEqual(scores.Count(), count);
         }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ReturnRequestedCount(int count)
+        {
+            const int PAGE = 0; // take the first page of results
+
+            // Fetch the scores.
+            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
+                score => true, // return all scores
+                score => 1, // we don't care about the order
+                PAGE,
+                count // fetch this number of results
+            );
+            IEnumerable<Score> scores = scoresTask.Result;
+
+            // Verify that we received the specified number of items.
+            Assert.AreEqual(scores.Count(), count);
+        }
     }
 }
