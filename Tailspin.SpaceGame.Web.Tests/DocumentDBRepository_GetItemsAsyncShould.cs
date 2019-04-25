855d95bed34b57d9dcefb6201b59dc6d14e4c7a7
Thomas Petchel <tpetchel@gmail.com>
2019-04-25 12:05:23 -0700

    Add failing test

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -49,67 +49,7 @@ namespace Tests
             IEnumerable<Score> scores = scoresTask.Result;
 
             // Verify that each score's game region matches the provided game region.
-            Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == "ABCD"));
-        }
-
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(10)]
-        public void ReturnRequestedCount(int count)
-        {
-            const int PAGE = 0; // take the first page of results
-
-            // Fetch the scores.
-            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
-                score => true, // return all scores
-                score => 1, // we don't care about the order
-                PAGE,
-                count // fetch this number of results
-            );
-            IEnumerable<Score> scores = scoresTask.Result;
-
-            // Verify that we received the specified number of items.
-            Assert.AreEqual(scores.Count(), count);
-        }
-
-        [TestCase(0, ExpectedResult = 0)]
-        [TestCase(1, ExpectedResult = 1)]
-        [TestCase(10, ExpectedResult = 10)]
-        public int ReturnRequestedCount(int count)
-        {
-            const int PAGE = 0; // take the first page of results
-
-            // Fetch the scores.
-            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
-                score => true, // return all scores
-                score => 1, // we don't care about the order
-                PAGE,
-                count // fetch this number of results
-            );
-            IEnumerable<Score> scores = scoresTask.Result;
-
-            // Verify that we received the specified number of items.
-            return scores.Count();
-        }
-
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(10)]
-        public void ReturnRequestedCount(int count)
-        {
-            const int PAGE = 0; // take the first page of results
-
-            // Fetch the scores.
-            Task<IEnumerable<Score>> scoresTask = _scoreRepository.GetItemsAsync(
-                score => true, // return all scores
-                score => 1, // we don't care about the order
-                PAGE,
-                count // fetch this number of results
-            );
-            IEnumerable<Score> scores = scoresTask.Result;
-
-            // Verify that we received the specified number of items.
-            Assert.AreEqual(scores.Count(), count);
+            Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
         }
 
         [TestCase(0, ExpectedResult = 0)]
