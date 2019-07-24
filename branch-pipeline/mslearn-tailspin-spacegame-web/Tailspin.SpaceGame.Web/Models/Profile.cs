74a6b1b6070af7b61a47e93abfa9b8cd2b55e948
Arnab <arnab.chakraborty@philips.com>
2019-07-24 15:28:15 +0530

    commit

---
+++ branch-pipeline/mslearn-tailspin-spacegame-web/Tailspin.SpaceGame.Web/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TailSpin.SpaceGame.Web.Models
+{
+    public class Profile : Model
+    {
+        // The player's user name.
+        [JsonProperty(PropertyName = "userName")]
+        public string UserName { get; set; }
+
+        // The URL of the player's avatar image.
+        [JsonProperty(PropertyName = "avatarUrl")]
+        public string AvatarUrl { get; set; }
+
+        // The achievements the player earned.
+        [JsonProperty(PropertyName = "achievements")]
+        public string[] Achievements { get; set; }
+    }
+}
