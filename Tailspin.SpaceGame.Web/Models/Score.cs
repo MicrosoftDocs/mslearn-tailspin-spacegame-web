06f29e35db82f72fe4c36e5dbc7e5a99b48ee047
Warrick <UA33991@DOW.COM>
2019-09-13 14:13:33 -0400

    add publish tasks

---
+++ Tailspin.SpaceGame.Web/Models/Score.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace TailSpin.SpaceGame.Web.Models
+{
+    public class Score : Model
+    {
+        // The ID of the player profile associated with this score.
+        [JsonProperty(PropertyName = "profileId")]
+        public string ProfileId { get; set; }
+
+        // The score value.
+        [JsonProperty(PropertyName = "score")]
+        public int HighScore { get; set; }
+
+        // The game mode the score is associated with.
+        [JsonProperty(PropertyName = "gameMode")]
+        public string GameMode { get; set; }
+
+        // The game region (map) the score is associated with.
+        [JsonProperty(PropertyName = "gameRegion")]
+        public string GameRegion { get; set; }
+    }
+}
