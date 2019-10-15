27ea1a388a69d6e116c2d3a008441667c947fce2
raysaik <saikat_ray@outlook.com>
2019-10-15 07:17:03 +0100

    changetest

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -8,6 +8,7 @@ namespace TailSpin.SpaceGame.Web
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            //baler
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
