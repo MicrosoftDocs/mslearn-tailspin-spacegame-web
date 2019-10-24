84a5c3cd817be4f34aa786ce1438a6e755207094
vipinpjohn <vipin.p.john@gmail.com>
2019-10-24 13:02:14 +0530

    Vipin John

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -29,8 +29,7 @@ namespace TailSpin.SpaceGame.Web.Controllers
             int page = 1, 
             int pageSize = 10, 
             string mode = "",
-            string region = "",
-             string region1 = 22
+            string region = ""
             )
         {
             // Create the view model with initial values we already know.
