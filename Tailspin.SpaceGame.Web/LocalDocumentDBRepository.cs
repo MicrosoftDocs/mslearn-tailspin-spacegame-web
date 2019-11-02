32ec55e8dc3e298656bf490ce53d7f26fe75a246
Yuriy-Margulis <57069113+Yuriy-Margulis@users.noreply.github.com>
2019-11-02 11:58:25 -0400

    fixed page size

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -61,7 +61,7 @@ namespace TailSpin.SpaceGame.Web
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1) // take items
+                .Take(pageSize) // take items
                 .AsEnumerable(); // make enumeratable
 
             return Task<IEnumerable<T>>.FromResult(result);
