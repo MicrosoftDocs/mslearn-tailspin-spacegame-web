b4be888bf174513ca044d12945bc79506a5d39a6
Darkaru <nikospina9611@gmail.com>
2019-11-27 16:08:43 -0500

    Actualización .NET 3.0

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -1,17 +1,43 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TailSpin.SpaceGame.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+	public class Program
+	{
+	public static void Main(string[] args)
+	{
+		CreateHostBuilder(args).Build().Run();
+	}
+	
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStartup<Startup>();
+			});
+	//	public static void Main(string[] args)
+	//	{
+	//		CreateHostBuilder(args).Build().Run();
+	//	}
+	//	public static IHostBuilder CreateHostBuilder(string[] args) =>
+	//		Host.CreateDefaultBuilder(args)
+	//			.ConfigureWebHostDefaults(webBuilder =>
+	//			{
+	//				webBuilder.ConfigureKestrel(serverOptions =>
+	//				{
+	//					// Set properties and call methods on options
+	//				})
+	//				.UseStartup<Startup>();
+	//			});
+		//public static IHostBuilder CreateHostBuilder(string[] args) =>
+		//	Host.CreateDefaultBuilder(args)
+		//		.ConfigureWebHostDefaults(webBuilder =>
+		//		{
+		//			webBuilder.UseStartup<Startup>();
+		//		});
+	}
 }
--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TailSpin.SpaceGame.Web.Models;
+using Microsoft.Extensions.Hosting;
 
 namespace TailSpin.SpaceGame.Web
 {
@@ -20,22 +20,24 @@ namespace TailSpin.SpaceGame.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddRazorPages();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            //MvcOptions.EnableEndpointRouting = false;
+            //services.AddMvc();            
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc(option => option.EnableEndpointRouting = false);
             // Add document stores. These are passed to the HomeController constructor.
             services.AddSingleton<IDocumentDBRepository<Score>>(new LocalDocumentDBRepository<Score>(@"SampleData/scores.json"));
             services.AddSingleton<IDocumentDBRepository<Profile>>(new LocalDocumentDBRepository<Profile>(@"SampleData/profiles.json"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -50,13 +52,25 @@ namespace TailSpin.SpaceGame.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+			
+			
+			app.UseRouting();
+			//app.UseCors();
+			app.UseAuthentication();
+			app.UseAuthorization();
+			
+			app.UseEndpoints(endpoints =>
+			{
+				//endpoints.MapHub<ChatHub>("/chat");
+				endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+			});
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
