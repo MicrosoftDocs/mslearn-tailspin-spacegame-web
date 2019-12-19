90d22c374df6050dd11af1ae0404d15ab2a0c909
Constantin Alexa <alexa_constantin@yahoo.com>
2019-12-19 16:31:21 +0200

    Run and publish unit tests

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -29,6 +29,7 @@ namespace Tests
         [TestCase("Pinwheel")]
         [TestCase("NGC 1300")]
         [TestCase("Messier 82")]
+        [TestCase("Should not work")]
         public void FetchOnlyRequestedGameRegion(string gameRegion)
         {
             const int PAGE = 0; // take the first page of results
