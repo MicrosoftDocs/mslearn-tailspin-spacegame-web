354fa41d7969ae5fce2fa931a09758abecca7b73
partha37 <parthi37cse@gmail.com>
2020-10-21 11:24:20 +0530

    commit

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -52,7 +52,7 @@ namespace TailSpin.SpaceGame.Web
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
