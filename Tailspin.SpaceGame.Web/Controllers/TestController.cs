9f34e98769a9ebd1a655856c3b9e3e3f32d44cb1
Jason <jianson.ca@gmail.com>
2021-01-10 16:42:13 -0500

    [MLN-001] New controller for test.

---
+++ Tailspin.SpaceGame.Web/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tailspin.SpaceGame.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private const string TEST_VALUE = "test value";
+
+        public TestController()
+        {
+           
+        }
+
+        [HttpGet()]
+        [Route("/acounts/{id}")]
+        public async Task<IActionResult> GetAccountById(string id)
+        {
+            try
+            {
+                return Ok(TEST_VALUE);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("/");
+            }
+        }
+    }
+}
