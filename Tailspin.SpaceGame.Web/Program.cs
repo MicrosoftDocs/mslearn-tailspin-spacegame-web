eced06c60c4d822f9024207fbac892260e87b9a4
Sam Vad <samanthvadlamudy9@outlook.com>
2021-03-09 10:59:35 -0700

    hjh

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -7,6 +7,7 @@ namespace TailSpin.SpaceGame.Web
     {
         public static void Main(string[] args)
         {
+            //TEST
             CreateWebHostBuilder(args).Build().Run();
         }
 
