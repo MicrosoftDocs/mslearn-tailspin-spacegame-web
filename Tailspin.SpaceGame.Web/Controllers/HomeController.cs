e0b8f46c7bc985ce184c7fff40f4125277f28078
rickmcguire <rwmcguire@yahoo.com>
2021-03-09 15:16:37 -0600

    Add reference to Models package

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using TailSpin.SpaceGame.Web.Models;
+//using TailSpin.SpaceGame.Web.Models;
 
 namespace TailSpin.SpaceGame.Web.Controllers
 {
