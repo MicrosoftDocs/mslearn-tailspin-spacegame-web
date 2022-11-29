0952cc658ba54c2d9d228749c3d43c5dae657148
Tejpal <tejpal.chohan@gmail.com>
2022-11-30 07:23:09 +1100

    npm-sass version

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -12,6 +12,6 @@ namespace TailSpin.SpaceGame.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
     }
 }
