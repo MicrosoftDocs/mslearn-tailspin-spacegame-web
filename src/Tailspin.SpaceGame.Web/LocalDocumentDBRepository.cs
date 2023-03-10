326e3eea34716f4fab954e5b84ec254bbdd84814
Miguel de Diego <miguel.de.diego@hotmail.es>
2023-03-10 20:57:32 +0100

    Update solution to include test project

--- src/Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ src/Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -18,6 +18,11 @@ namespace TailSpin.SpaceGame.Web
             // Serialize the items from the provided JSON document.
             _items = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(fileName));
         }
+        public LocalDocumentDBRepository(Stream stream)
+        {
+            // Serialize the items from the provided JSON document.
+            _items = JsonSerializer.Deserialize<List<T>>(new StreamReader(stream).ReadToEnd());
+        }
 
         /// <summary>
         /// Retrieves the item from the store with the given identifier.
