70701b77dca7fc2dbfee622473bd5ff3edd6b53e
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2023-03-15 17:30:29 -0400

    update dev container and move files (#7148)

--- .devcontainer/library-scripts/start.sh
+++ .devcontainer/library-scripts/start.sh
@@ -1,6 +1,9 @@
 #start.sh
 #!/bin/bash
-# Pulled from GitHub Codespace secrets
+
+#Script modified from https://github.com/Pwd9000-ML/GitHub-Codespaces-Lab/tree/master/.devcontainer/codespaceADOagent
+
+#GitHub Codespace secrets
 ADO_ORG=$ADO_ORG
 ADO_PAT=$ADO_PAT
 ADO_POOL_NAME=$ADO_POOL_NAME
@@ -11,7 +14,6 @@ AGENT_SUFFIX="ADO-agent"
 AGENT_NAME="${HOSTNAME}-${AGENT_SUFFIX}"
 ADO_URL="https://dev.azure.com/${ADO_ORG}"
 
-# !!!Ignore sensitive tokens from capabilities!!!
 export VSO_AGENT_IGNORE=ADO_PAT,GH_TOKEN,GITHUB_CODESPACE_TOKEN,GITHUB_TOKEN
 
 /home/vscode/azure-pipelines/config.sh --unattended \
@@ -19,7 +21,8 @@ export VSO_AGENT_IGNORE=ADO_PAT,GH_TOKEN,GITHUB_CODESPACE_TOKEN,GITHUB_TOKEN
 --url "${ADO_URL}" \
 --auth PAT \
 --token "${ADO_PAT}" \
---pool "${ADO_POOL_NAME}" \
+--pool "${ADO_POOL_NAME:-Default}" \
+--replace \
 --acceptTeeEula
 
 /home/vscode/azure-pipelines/run.sh
