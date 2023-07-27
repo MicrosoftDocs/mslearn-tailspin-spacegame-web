1876d227d5b638ee8b1d869791a80facc859ee2b
muralikrishna188 <muralid188@outlook.com>
2023-07-27 16:18:00 +0530

    update

---
+++ Infra/variables.tf
@@ -0,0 +1,24 @@
+variable "subscription_id" {
+    type = string
+    default = "049cc0b9-2696-4e70-871b-4366be487c19"
+    description = "dev subscription"
+  
+}
+variable "client_id" {
+    type = string
+    default = "f1ea2e72-ee23-4374-bdea-5df06ac7cfaa"
+    description = "dev client id"
+  
+}
+variable "client_secret" {
+    type = string
+    default = "4EK8Q~h6~sJFQSD-Fn8LpBeAQRdNJ5nQcgD-xaJQ"
+    description = "dev client secret"
+  
+}
+variable "tenant_id" {
+    type = string
+    default = "477cf0a5-266c-4331-8f0a-865f4622d888"
+    description = "dev tenant id"
+  
+}
