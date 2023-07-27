dde0768e414650ff578e102c1640f148d26e7b6e
muralikrishna188 <muralid188@outlook.com>
2023-07-27 16:59:41 +0530

    updte

--- Infra/main.tf
+++ Infra/main.tf
@@ -20,8 +20,8 @@ provider "azurerm" {
 locals {
   setup_name = "practice-hyd"
 }
-resource "azurerm_resource_group" "appservicerglabel1" {
-    name = "appservicerg1287"
+resource "azurerm_resource_group" "appservicerglabel12" {
+    name = "appservicdn18"
     location = "East US"
     tags = {
       "name" = "${local.setup_name}-rsg"
@@ -29,14 +29,14 @@ resource "azurerm_resource_group" "appservicerglabel1" {
 }
 resource "azurerm_app_service_plan" "appplanlabel11" {
     name = "appplan11"
-    location = azurerm_resource_group.appservicerglabel1.location
-    resource_group_name = azurerm_resource_group.appservicerglabel1.name
+    location = azurerm_resource_group.appservicerglabel12.location
+    resource_group_name = azurerm_resource_group.appservicerglabel12.name
     sku {
         tier = "standard"
         size = "S1"
     }
     depends_on = [
-      azurerm_resource_group.appservicerglabel1
+      azurerm_resource_group.appservicerglabel12
     ]
     tags = {
       "name" = "${local.setup_name}-appplan"
@@ -45,8 +45,8 @@ resource "azurerm_app_service_plan" "appplanlabel11" {
 }
 resource "azurerm_app_service" "webapplabell1" {
     name = "webapp12581"
-    resource_group_name = azurerm_resource_group.appservicerglabel1.name
-    location = azurerm_resource_group.appservicerglabel1.location
+    resource_group_name = azurerm_resource_group.appservicerglabel12.name
+    location = azurerm_resource_group.appservicerglabel12.location
     app_service_plan_id = azurerm_app_service_plan.appplanlabel11.id
     depends_on = [
       azurerm_app_service_plan.appplanlabel11
