987feae9276163a0351d40fec3037fc4ce80f1cd
Logeswaran25 <102804587+Logeswaran25@users.noreply.github.com>
2024-02-03 14:18:21 +0530

    Set up CI with Azure Pipelines
    [skip ci]

---
+++ Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TailSpin.SpaceGame.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
