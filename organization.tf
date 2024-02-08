5d63b1f2986b9c9cb17fb1e4578785a3a68b089c
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-08 16:33:02 -0300

    pleura inflamada

--- organization.tf
+++ organization.tf
@@ -1,5 +1,10 @@
 resource "aws_organizations_account" "account" {
-  name  = "jujuba"
-  email = "matheushfbrandao@outlook.com"
-}
+  name      = "my_new_account"
+  email     = "john@doe.org"
+  role_name = "myOrganizationRole"
 
+  # There is no AWS Organizations API for reading role_name
+  lifecycle {
+    ignore_changes = [aws_organizations_account.account.role_name]
+  }
+}
