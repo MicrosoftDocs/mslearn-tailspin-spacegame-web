926d9f9fb2c16f49eb5efae42e2063e0e88a6300
Matheus Henrique Ferreira Brandão <matheus.brandao@stoque.com.br>
2024-02-08 10:48:50 -0300

    teste terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cliente" {
+  default = ""
+  description = "Nome do cliente"
+}
+
+variable "environment" {
+  default = "dev"
+  description = "Nome do ambiente que pode ser dev, stage ou prod"
+}
