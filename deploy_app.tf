bbbd56f1e468bb1d1ce9c80a9e97358547be12c1
Breon.Green@globalpay.com <Breon.Green@globalpay.com>
2024-12-05 03:25:18 -0500

    add tf cod

--- deploy_app.tf
+++ deploy_app.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.12.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstatestgacct27555"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
