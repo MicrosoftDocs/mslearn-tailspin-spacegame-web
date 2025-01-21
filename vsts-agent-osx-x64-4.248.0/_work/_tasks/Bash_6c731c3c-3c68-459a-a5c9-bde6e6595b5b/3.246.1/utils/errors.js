f6352faeeb69be196f64a522357716b43e2266dc
A00467990x <144293496+A00467990x@users.noreply.github.com>
2025-01-21 08:32:38 -0400

    Add the build configuration

--- vsts-agent-osx-x64-4.248.0/_work/_tasks/Bash_6c731c3c-3c68-459a-a5c9-bde6e6595b5b/3.246.1/utils/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ArgsSanitizingError = void 0;
-class ArgsSanitizingError extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.ArgsSanitizingError = ArgsSanitizingError;
